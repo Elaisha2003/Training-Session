31d76fc7c3b70a7043445f83a80809aeb6116eee
Elaisha2003 <elaishamothi@gmail.com>
2024-08-30 17:12:12 +0530

    webpage grid

---
+++ Json/index2.js
@@ -0,0 +1,33 @@
+// a = [1,4,6,2,7]
+
+
+// double=a.map(e=>e**2)
+// console.log(double)
+
+// even=a.filter(e=>e%2==0)
+// console.log(even)
+
+// max=a.reduce((i,e)=>i+e,0)
+// console.log(max)
+
+// a.forEach(e =>console.log(e));
+
+
+
+// products = [ 
+//  { name: "Laptop",price: 1200, inStock: true },
+//  { name: "Phone",price: 800, inStock: false }, 
+//  { name: "Tablet", price: 600, inStock:true }, 
+//  { name:"Monitor", price: 300, inStock: true } ];
+
+
+//  m=products.map(e=>e.name)
+//  console.log(m)
+
+//  f=products.filter(e=>(e.inStock==true))
+//  console.log(f)
+
+//  r=products.reduce((i,e)=>i+e.price,0)
+//  console.log(r)
+
+//  products.forEach(e=>console.log(e))
